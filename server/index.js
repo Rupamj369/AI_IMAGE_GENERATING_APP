890b422f5b0968fa64d17d7962343ff9d7bdbf7a
Rupam <coderrupam@gmail.com>
2023-07-12 07:09:00 +0530

    firest commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import postRoutes from "./Routes/postRoutes.js";
+import dalleRoutes from "./Routes/dalleRoutes.js";
+import connetDB from "./mongodb/connect.js";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E!");
+});
+
+const startServer = async () => {
+  try {
+    connetDB(process.env.MONGODB_URL);
+  } catch (error) {
+    console.log(error);
+  }
+  app.listen(8080, () =>
+    console.log("Server has started on port http://localhost:8080")
+  );
+};
+startServer();
